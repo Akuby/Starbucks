515b838d331c3c9379c63c5028da97acc8a178d1
Hyewon Ryu <akuby1997@gmail.com>
2022-02-11 18:01:48 +0900

    visual section 완성

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ searchInputEl.addEventListener('blur', function(){
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', _.throttle(function () {
+window.addEventListener('scroll', _.throttle(function () { //throttle(함수, 시간)
   console.log(window.scrollY);
   if (window.scrollY > 500) {
     //배지 숨기기
@@ -35,3 +35,14 @@ window.addEventListener('scroll', _.throttle(function () {
     });
   }
 }, 300));
+
+//visual fade-in
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  //gsap.to(요소, 지속시간, 옵션)
+  gsap.to(fadeEl, 1, {
+    delay: (index+1) * .7, //각각 0.7, 1.4, 2.1, 2.7 초 후에 실행
+    opacity: 1
+  });
+});
