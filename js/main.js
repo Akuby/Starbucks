c17acfd7129a487723184b7c32d2b579647144f2
Hyewon Ryu <akuby1997@gmail.com>
2022-02-16 16:48:54 +0900

    promotion 구현 및 toggleBtn 활성화

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ searchInputEl.addEventListener('blur', function(){
 //우측 배지
 const badgeEl = document.querySelector('header .badges');
 window.addEventListener('scroll', _.throttle(function () { //throttle(함수, 시간)
-  console.log(window.scrollY);
   if (window.scrollY > 500) {
     //배지 숨기기
     //gsap.to(요소, 지속시간, 옵션)
@@ -42,10 +41,12 @@ const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
   //gsap.to(요소, 지속시간, 옵션)
   gsap.to(fadeEl, 1, {
-    delay: (index+1) * .7, //각각 0.7, 1.4, 2.1, 2.7 초 후에 실행
+    delay: (index+1) * .55, //각각 0.7, 1.4, 2.1, 2.7 초 후에 실행
     opacity: 1
   });
 });
+
+//공지사항 스와이퍼
 
 // new Swiper(선택자, 옵션)
 new Swiper('.notice-line .mySwiper', {
@@ -53,3 +54,37 @@ new Swiper('.notice-line .mySwiper', {
   autoplay: true,
   loop : true
 });
+
+//프로모션 스와이퍼
+new Swiper(".promotion .mySwiper", {
+  slidesPerView : 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween : 10, //슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay : 5000
+  },
+  pagination : {
+    el : '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable : true //번호 요소 제어 가능 여부
+  },
+  navigation : {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+//프로모션 토글
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    // 숨김
+    promotionEl.classList.add('hide');
+  } else {
+    //보임
+    promotionEl.classList.remove('hide');
+  }
+})
