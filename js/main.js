5f3ebace856784e3bd858c644995fb9764a578c0
Akuby <akuby1997@gmail.com>
2022-02-24 20:36:58 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-//검색영역 선택 시
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input'); // 검색영역 > input, span
-const searchSpanEl = searchEl.querySelector('span'); // 검색어 입력 영역
-
-searchSpanEl.addEventListener('mouseover', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
 //우측 배지
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -149,7 +130,3 @@ spyEls.forEach(function(spyEl){
       .setClassToggle(spyEl, 'show')
       .addTo(new ScrollMagic.Controller());
 });
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
